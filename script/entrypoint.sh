d8d5cee045a39e01f5b68a41e1a031f1c4c1c7cf
Bobby Burton <bobby.burton@protonmail.com>
2022-06-19 09:10:54 +0100

    .

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 name=$(date '+%Y-%m-%d_%H-%M-%S')
-tar -zcv /home/minecraft/save/world /home/minecraft/save/world_nether /home/minecraft/save/world_the_end /home/minecraft/save/whitelist.json | pv -L 3m > "world-$name.tar.gz"
+tar -zcv /home/minecraft/save/world /home/minecraft/save/world_nether /home/minecraft/save/world_the_end /home/minecraft/whitelist.json | pv -L 3m > "world-$name.tar.gz"
 s3cmd -c /home/backup/s3cfg put world-$name.tar.gz s3://nerdrealm/ltn/world-$name-tar.gz
